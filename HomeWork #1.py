bf5192732206b7313338faec55355a83d555d432
Andrew <anryzhakov@yandex.ru>
2024-09-25 19:39:34 +0300

    Add files via upload

---
+++ HomeWork #1.py
@@ -0,0 +1,15 @@
+# Слушатель курса Junior Python-разработчик
+# Рыжаков Андрей Николаевич, anryzhakov@yandex.ru
+# Домашняя работа по уроку "Модули и пакеты"
+
+import fake_math as fm
+import true_math as tm
+
+result1 = fm.divide(69, 3)
+print(result1)
+result2 = fm.divide(3, 0)
+print(result2)
+result3 = tm.divide(49, 7)
+print(result3)
+result4 = tm.divide(15, 0)
+print(result4)
